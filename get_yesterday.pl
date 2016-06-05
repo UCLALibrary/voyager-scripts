ea2602f58ed370f759966bac982fa01b36544254
Andy Kohler <akohler@library.ucla.edu>
2016-06-04 21:56:30 -0700

    Add get_yesterday.pl, used in vger_circjobs

---
+++ get_yesterday.pl
@@ -0,0 +1,14 @@
+#!/bin/perl
+
+($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(( time() - ( 24 * 60 * 60 ) ));
+$mon += 1;
+$year += 1900;
+
+if ($mon < 10 ) {
+	$mon = '0'.$mon;
+}
+if ($mday < 10 ) {
+	$mday = '0'.$mday;
+}
+$output = $year.$mon.$mday;
+print "$output\n";
