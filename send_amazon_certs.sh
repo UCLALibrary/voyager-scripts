4ff8ac27a589f693bff4ef8339a128fac6987e8d
Andy Kohler <akohler@library.ucla.edu>
2016-06-22 19:08:16 -0700

    VBT-570: Change references to /usr/local/bin/java

--- send_amazon_certs.sh
+++ send_amazon_certs.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+# Get voyager environment, for vars and for cron
+. `echo $HOME | sed "s/$LOGNAME/voyager/"`/.profile.local
+
 LC_CTYPE=en_US.UTF-8
 export LC_CTYPE
 
-JAVA=/usr/local/bin/java
+JAVA=${BIN}/java
 DIR=/usr/local/java
 
 if [ ! -x ${JAVA} ]
